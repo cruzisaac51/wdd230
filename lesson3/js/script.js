65c92cca232f8554766488400c807047835dc86f
cruzisaac51 <cruzisaac51@gmail.com>
2021-10-06 09:25:39 -0400

    fix errors in lesson3, lesson4 created prerstonpg

--- lesson3/js/script.js
+++ lesson3/js/script.js
@@ -6,3 +6,8 @@ document.querySelector("#dateyear").textContent = fulldate;
 
 let oLastModif = new Date(document.lastModified)
 document.querySelector("#lastUpdated").textContent = oLastModif;
+
+
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
---
+++ lesson4/js/script.js
@@ -0,0 +1,18 @@
+const d = new Date();
+const year = d.getFullYear();
+const fulldate = `${year}`;
+document.querySelector("#dateyear").textContent = fulldate;
+
+// const day = d.getDay();
+// const month = d.getMonth();
+// const fdate = `${day}/${month}/${year}`;
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+const fdate = d.toLocaleDateString("en-US", options);
+document.querySelector("#lastUpdated").textContent = fdate;
+//     let oLastModif = new Date(document.lastModified)
+// document.querySelector("#lastUpdated").textContent = fdate;
+
+
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
