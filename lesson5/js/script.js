0d185f1f4e1ebae5f49dab266937474b4bdadde2
cruzisaac51 <cruzisaac51@gmail.com>
2021-10-15 01:16:31 -0500

    Book of mormon site

---
+++ lesson5/js/script.js
@@ -0,0 +1,35 @@
+const list = document.querySelector("ul");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+
+
+button.onclick = function() {
+    let myItem = input.value;
+    input.value = "";
+
+    const listItem = document.createElement("li");
+    const listText = document.createElement("span");
+    const listBtn = document.createElement("button");
+
+    listItem.appendChild(listText);
+    listText.textContent = myItem;
+    listItem.appendChild(listBtn);
+    listBtn.textContent = "❌";
+    list.appendChild(listItem);
+
+    listBtn.onclick = function() {
+        list.removeChild(listItem);
+    }
+
+    input.focus();
+
+    const inputvalue = () => {
+        if (myItem == null || myItem.length == 0 || /^\s+$/.test(myItem)) {
+            alert('[ERROR] The form field cannot be empty');
+            list.removeChild(listItem);
+            return false;
+        }
+    }
+
+    inputvalue();
+}
