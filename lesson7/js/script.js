2f9d054f129d5a0bc3d85cee49806d802d3a1e99
Isaac Cruz <cruzisaac51@gmail.com>
2021-10-29 15:51:38 -0500

    Lazy load images activity and bases for preston 7

---
+++ lesson7/js/script.js
@@ -0,0 +1,39 @@
+const banner = document.getElementById("banner");
+let today = new Date();
+day = today.getDay();
+
+const showBanner = () => {
+    banner.style.display = "block";
+};
+
+if (day == 5) {
+    showBanner();
+}
+
+
+const d = new Date();
+const year = d.getFullYear();
+const fulldate = `${year}`;
+document.querySelector("#dateyear").textContent = fulldate;
+
+// const day = d.getDay();
+// const month = d.getMonth();
+// const fdate = `${day}/${month}/${year}`;
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+const fdate = d.toLocaleDateString("en-UK", options);
+document.querySelector("#lastUpdated").textContent = fdate;
+//     let oLastModif = new Date(document.lastModified)
+// document.querySelector("#lastUpdated").textContent = fdate;
+
+
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+
+WebFont.load({
+    google: {
+        families: [
+            'krub'
+        ]
+    }
+});
