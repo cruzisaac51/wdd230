ea91818c7f4e0f25b159f38568542153f7e1d4f5
Isaac Cruz <cruzisaac51@gmail.com>
2021-10-30 18:15:39 -0500

    Gallery page done

--- lesson7/js/script.js
+++ lesson7/js/script.js
@@ -16,24 +16,66 @@ const year = d.getFullYear();
 const fulldate = `${year}`;
 document.querySelector("#dateyear").textContent = fulldate;
 
-// const day = d.getDay();
-// const month = d.getMonth();
-// const fdate = `${day}/${month}/${year}`;
 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 const fdate = d.toLocaleDateString("en-UK", options);
 document.querySelector("#lastUpdated").textContent = fdate;
-//     let oLastModif = new Date(document.lastModified)
-// document.querySelector("#lastUpdated").textContent = fdate;
 
 
 function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
 
-WebFont.load({
-    google: {
-        families: [
-            'krub'
-        ]
-    }
+
+// lazy load 
+
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const loadImages = (image) => {
+	image.setAttribute("src", image.getAttribute("data-src"));
+	image.onload = () => {
+		image.removeAttribute("data-src");
+	};
+};
+if ("IntersectionObserver" in window) {
+	const observer = new IntersectionObserver((items, observer) => {
+		items.forEach((item) => {
+			if (item.isIntersecting) {
+				loadImages(item.target);
+				observer.unobserve(item.target);
+			}
+		});
+	});
+	imagesToLoad.forEach((img) => {
+		observer.observe(img);
+	});
+} else {
+	imagesToLoad.forEach((img) => {
+		loadImages(img);
+	});
+}
+// locale storage
+
+const daysVisited = document.getElementById("container");
+const timestamp = today.getTime();
+
+document.addEventListener("DOMContentLoaded", () => {
+  populateStorage();
 });
+
+const populateStorage = () => {
+  try {
+    let theDay = localStorage.getItem("daysVisited");
+    if (theDay != timestamp) {
+      // (1.000 miliseconds x 60 seconds x 60 minutes x 24 hours)
+      let days = Math.round((timestamp - theDay) / 86400000);
+      daysVisited.textContent = `Amount of time in days between user visits: ${days} days`;
+      setStyles();
+    }
+  } catch (er) {
+    setStyles();
+  }
+};
+
+const setStyles = () => {
+  localStorage.setItem("daysVisited", timestamp);
+};
