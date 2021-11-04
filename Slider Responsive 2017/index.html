f0014536f59bace75d0a24644a2153eee692431e
Isaac Cruz <cruzisaac51@gmail.com>
2021-11-04 02:08:05 -0600

    modify the home page

---
+++ Slider Responsive 2017/index.html
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title>Slider Responsive 2017</title>
+
+	<link rel="stylesheet" href="css/flexslider.css">
+	<link rel="stylesheet" href="css/font-awesome.css">
+
+	<script src="js/jquery-3.1.0.min.js"></script>
+	<script src="js/jquery.flexslider.js"></script>
+	<script src="js/main.js"></script>
+</head>
+<body>
+	<h1 id="page-title">Slider Responsive con Flexslider</h1>
+	<div class="flexslider">
+		<ul class="slides">
+
+			<li>
+				<img src="img/1.jpg" alt="">
+				<section class="caption">
+					<h2>Lorem slider #1</h2>
+				</section>
+			</li>
+
+			<li>
+				<img src="img/2.jpg" alt="">
+				<section class="caption">
+					<h2>Lorem slider #2</h2>
+				</section>
+			</li>
+
+			<li>
+				<img src="img/3.jpg" alt="">
+				<section class="caption">
+					<h2>Lorem slider #3</h2>
+				</section>
+			</li>
+
+		</ul>
+	</div>
+
+</body>
+</html>
--- js/script.js
+++ js/script.js
@@ -3,24 +3,41 @@ const year = d.getFullYear();
 const fulldate = `${year}`;
 document.querySelector("#dateyear").textContent = fulldate;
 
-// const day = d.getDay();
-// const month = d.getMonth();
-// const fdate = `${day}/${month}/${year}`;
 const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 const fdate = d.toLocaleDateString("en-UK", options);
 document.querySelector("#lastUpdated").textContent = fdate;
-//     let oLastModif = new Date(document.lastModified)
-// document.querySelector("#lastUpdated").textContent = fdate;
+
 
 
 function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
 
-WebFont.load({
-    google: {
-        families: [
-            'krub'
-        ]
-    }
+// less slider navigation
+$(document).ready(function(){
+
+	$('.flexslider').flexslider({
+		prevText: "",
+		nextText: "",
+		pauseOnAction: false,
+		slideshowSpeed: 3000,
+		touch: true
+	});
+
 });
+
+document.addEventListener("click", e => {
+    const isDropdownButton = e.target.matches("[data-dropdown-button]")
+    if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return
+  
+    let currentDropdown
+    if (isDropdownButton) {
+      currentDropdown = e.target.closest("[data-dropdown]")
+      currentDropdown.classList.toggle("active")
+    }
+  
+    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+      if (dropdown === currentDropdown) return
+      dropdown.classList.remove("active")
+    })
+  })
